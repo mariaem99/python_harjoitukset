1fe864c6f1d6eccd5d90990f6edb7ad7024baa9a
Maria Erkkilä <mmaria.erkkila@gmail.com>
2026-09-01 14:28:28 +0300

    lisaa teht. 6

---
+++ mod02/teht6.py
@@ -0,0 +1,14 @@
+import random
+
+koodi1 = random.randint(0, 9)
+koodi2 = random.randint(0, 9)
+koodi3 = random.randint(0, 9)
+
+print("Kolmenumeroinen koodi:", koodi1, koodi2, koodi3)
+
+koodi4 = random.randint(1, 6)
+koodi5 = random.randint(1, 6)
+koodi6 = random.randint(1, 6)
+koodi7 = random.randint(1, 6)
+
+print("Nelinumeroinen koodi:", koodi4, koodi5, koodi6, koodi7)
