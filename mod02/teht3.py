c76cb33d1f60611ae7fe388a1578e0ae01033e74
Maria Erkkilä <mmaria.erkkila@gmail.com>
2026-09-01 10:59:06 +0300

    Lisaa teht 3

---
+++ mod02/teht3.py
@@ -0,0 +1,8 @@
+kanta = float(input("Anna kanta: "))
+korkeus = float(input("Anna korkeus: "))
+
+piiri = 2 * kanta + 2 * korkeus
+pinta_ala = kanta * korkeus
+
+print("Suorakulmion piiri on", piiri)
+print("Suorakulmion kanta on", pinta_ala)
